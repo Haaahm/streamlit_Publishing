c01d24bb6f252c50513491b619495c2041057341
Sean.Hahm <131845542+Haaahm@users.noreply.github.com>
2023-06-21 23:34:27 +0900

    Update pie_chart_example.py

--- pie_chart_example.py
+++ pie_chart_example.py
@@ -5,20 +5,20 @@ import streamlit as st
 import plotly.express as px
 
 # 제목 설정
-st.set_page_config(page_title='2022 유가증권시장 거래 현황')
-st.header('2022 유가증권시장 거래 현황')
-st.subheader('거래량 & 거래대금')
+st.set_page_config(page_title='투자운용사별 ETF 현황')
+st.header('투자운용사별 ETF 현황')
+st.subheader('상장종목수 & 순자산가치총액 & 일평균거래대금')
 
 # 데이터 불러오기
 excel_file = st.file_uploader(".xlsx 파일을 선택해주세요", type=['xlsx'])
 if excel_file is not None:
     # Excel 파일을 DataFrame으로 읽어오기
-    df = pd.read_excel(excel_file, sheet_name='Sheet1', usecols='G:K')
+    df = pd.read_excel(excel_file, sheet_name='Sheet1', usecols='A:F')
 
     # 그래프 설정
     colors = ['lightsteelblue', 'thistle', 'bisque']
-    chart_titles = ['총 거래량', '일평균 거래량', '총 거래대금', '일평균 거래대금']
-    value_columns = ['총 거래량', '일평균 거래량', '총 거래대금', '일평균 거래대금']
+    chart_titles = ['상장종목수', '순자산가치총액', '일평균거래대금']
+    value_columns = ['상장종목수', '순자산가치총액', '일평균거래대금']
 
     # 그래프 그리기
     for title, value_column in zip(chart_titles, value_columns):
